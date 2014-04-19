d60f3c89e0d56875feec0896d648c4f426bf5dd4
raticate <caterina.munoz.v@gmail.com>
2014-04-18 23:36:14 -0400

    khhk

--- src/tarea1Logaritmos/ArrayInstance.java
+++ src/tarea1Logaritmos/ArrayInstance.java
@@ -2,35 +2,42 @@ package tarea1Logaritmos;
 
 import java.util.Random;
 
-public class ArrayInstances {
-	Random random=new Random();
-	public int[] RandomGenerator(int size){
+public class ArrayInstance {
+	static Random random=new Random();
+	public static int[] RandomGenerator(int size){
 		int[] instance=new int[size]; //Crear arreglo
-		for(int i=0;i<size;i++) //Llenar arreglo
+		for(int i=0;i<size;i++){ //Llenar arreglo
 			instance[i]=i+1;
+		}
 		int aux,r;
 		for(int j=0;j<size;j++){ //Hacer size permutaciones aleatorias
-			r=random.nextInt(size)+1; //valor aleatorio entre 1 y size
-			aux=instance[j]; // permutar j+1 con el valor aleatorio
-			instance[j]=r;
-			instance[r]=aux;
+			r=random.nextInt(size); //valor aleatorio entre 0 y size-1
+			aux=instance[r];
+			instance[r]=instance[j];
+			instance[j]=aux;
 		}
 		return instance;
 		
 	}
-	public int[] AlmostSortedGenerator(int size ){
+	public static int[] AlmostSortedGenerator(int size ){
 		int perms=size/10; //cantidad de permutaciones que se van a hacer
 		int[] instance=new int[size]; //Crear arreglo
 		for(int i=0;i<size;i++) //Llenar arreglo
 			instance[i]=i+1;
 		int aux,r1,r2;
 		for(int j=0;j<perms;j++){ //Hacer size/10 permutaciones aleatorias
-			r1=random.nextInt(size)+1; //2 valores aleatorios entre 1 y size
-			r2=random.nextInt(size)+1;
+			r1=random.nextInt(size); //2 valores aleatorios entre 1 y size
+			r2=random.nextInt(size);
 			aux=instance[r1];  // permutar valores
-			instance[r1]=r2;
+			instance[r1]=instance[r2];
 			instance[r2]=aux;
 		}
 		return instance;	
+	}
+	public static String toString(int[] a){
+		String r="";
+		for(int i=0;i<a.length;i++)
+			r+=a[i]+",";
+		return r;
 	}
 }
